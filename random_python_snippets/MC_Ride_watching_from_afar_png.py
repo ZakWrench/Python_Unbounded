1e8b9a5b449b507c0d847b3d1a406e954f1d916d
Zak <112790950+ZakWrench@users.noreply.github.com>
2023-02-02 09:36:07 +0000

    Update MC_Ride_watching_from_afar_png.py

--- random_python_snippets/MC_Ride_watching_from_afar_png.py
+++ random_python_snippets/MC_Ride_watching_from_afar_png.py
@@ -1,4 +1,6 @@
-# Avoid these mistakes
+######
+
+# Avoid changing values of an iterable while iterating
 
 lst = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
@@ -12,3 +14,14 @@ print(lst)  # [20, 30, 50, 60, 80, 90]
 lst = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 new_lst = [v for i, v in enumerate(lst) if i % 2 == 0]
 print(new_lst)
+
+######
+
+# Circular import -> change filename from built-in name to fix it
+gooey.py
+import gooey
+>> AttributeError: partially initialized module 'gooey' has no attribute 'GooeyParser' (most likely due to a circular import)
+    
+    
+######
+
